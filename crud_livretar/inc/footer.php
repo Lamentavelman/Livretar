dd3410bd98dc1830026f0f3f7259b44175d99625
O Lamentavelman <paragabrielrodrigues@gmail.com>
2024-09-28 16:53:45 -0300

    Update footer.php

--- crud_livretar/inc/footer.php
+++ crud_livretar/inc/footer.php
@@ -2,7 +2,7 @@
 </main>
 	<footer class="container">
         <?php $hoje = new Datetime("now", new DatetimeZone("America/Sao_Paulo")); ?>
-		<p>&copy; 2023 a <?php echo $hoje->format("Y") ?> - Projeto SW1 - Bryan_Moedas e Authentic_Coin</p>
+		<p>&copy; 2023 a <?php echo $hoje->format("Y") ?> - Projeto SW1 - Bryan Vinicius e Gabriel Rodrigues(O Lamentavelman)</p>
 	</footer>
 
     <script src="<?php echo BASEURL; ?>js/jquery-3.7.0.min.js"></script>
